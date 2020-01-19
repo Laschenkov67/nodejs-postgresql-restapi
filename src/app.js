d82c36ee1f53866a7f3381c004846e47ff7b5866
artem laschenkov <laschenkov59@yandex.ru>
2020-01-19 21:22:09 +0300

    update routes

--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import express, { json } from 'express';
 import morgan from 'morgan';
 
+// Import routes
+import projectRoutes from './routes/projects';
+import taskRoutes from './routes/tasks'
+
 const app = express();
 
 // Middlewares
@@ -8,5 +12,7 @@ app.use(morgan('dev'));
 app.use(json());
 
 //Routes
+app.use('/api/projects', projectRoutes);
+app.use('/api/tasks', taskRoutes);
 
 export default app;
---
+++ src/routers/tasksRoutes.js
@@ -0,0 +1,5 @@
+import { Router } from 'express';
+
+const tascksRouter = Router();
+
+export default tascksRouter;
